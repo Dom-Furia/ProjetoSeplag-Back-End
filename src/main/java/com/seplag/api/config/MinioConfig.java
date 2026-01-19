58f20bdeaa2dc1e82b7db5469ce30156d850c03a
julio <juliocesar.ti.2015@hotmail.com>
2026-01-19 15:25:46 -0400

    feat: updating and adding new dependencies

--- src/main/java/com/seplag/api/config/MinioConfig.java
+++ src/main/java/com/seplag/api/config/MinioConfig.java
@@ -1,18 +1,16 @@
 package com.seplag.api.config;
 
 import io.minio.MinioClient;
-import io.minio.MinioProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-@EnableConfigurationProperties(MinioProperties.class)
-//Cria o MinioClient para ser injetado em qualquer serice
+// Cria o MinioClient para ser injetado em qualquer service
 public class MinioConfig {
+
     @Bean
-    public MinioClient minioClient(com.seplag.api.config.MinioProperties props) {
+    public MinioClient minioClient(MinioProperties props) {
         return MinioClient.builder()
                 .endpoint(props.getUrl())
                 .credentials(
