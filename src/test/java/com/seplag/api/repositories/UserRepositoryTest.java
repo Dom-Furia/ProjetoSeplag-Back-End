b5e89098a9007aa694465b544b0713c7fd5cc13c
juliocesar <juliocesar.ti.2015@hotmail.com>
2026-02-04 02:20:09 -0400

    feat:Integration test

--- src/test/java/com/seplag/api/repositories/UserRepositoryTest.java
+++ src/test/java/com/seplag/api/repositories/UserRepositoryTest.java
@@ -1,20 +1,52 @@
 package com.seplag.api.repositories;
 
+
 import com.seplag.api.domain.user.User;
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @DataJpaTest
 @ActiveProfiles("test")
 class UserRepositoryTest {
 
+    @Autowired
     EntityManager entityManager;
 
+    @Autowired
+    UserRepository userRepository;
+
+
     @Test
-    void findByEmail() {
+    @DisplayName("Should Get E-mail sucessfully from DB")
+    void findByEmailCase1() {
+
+        // Arrange (dado)
+        String email = "teste@email.com";
+        User user = new User();
+        user.setEmail(email);
+        user.setName("Usuário Teste");
+        user.setPassword("123456");
+
+        entityManager.persist(user);
+        entityManager.flush();
+
+        // Act (ação)
+        Optional<User> result = userRepository.findByEmail(email);
+
+        // Assert (verificação)
+        assertThat(result).isPresent();
+        assertThat(result.get().getEmail()).isEqualTo(email);
     }
+
+
 
 }
